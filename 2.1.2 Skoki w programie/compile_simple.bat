ff29edc27258a2757bc97650194f73964a94c5bb
dominik-chat <dominik.sz.chat@gmail.com>
2021-04-13 17:40:18 +0200

    Poprawa skryptow

--- 2.1.2 Skoki w programie/compile_simple.bat
+++ 2.1.2 Skoki w programie/compile_simple.bat
@@ -1,4 +1,4 @@
 @echo off
 for %%f in ("%1") do set filename=%%~nf
 
-arm-none-eabi-gcc -Wall -O0 -mcpu=arm7tdmi-s -marm  --specs=nosys.specs %1 -o %filename%.o  
+arm-none-eabi-gcc -Wall -O0 -mcpu=arm7tdmi-s -marm  --specs=nosys.specs %1 -o %filename%.elf
--- Linker i Startup/Raw i (pll, mam, wektory przerwan)/clean.bat
+++ Linker i Startup/Raw i (pll, mam, wektory przerwan)/clean.bat
@@ -4,3 +4,5 @@ del *.o 2> nul
 del *.elf 2> nul
 del *.hex 2> nul
 del *.asm 2> nul
+del *.map 2> nul
+del *.tab 2> nul
